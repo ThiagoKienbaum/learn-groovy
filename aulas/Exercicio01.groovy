e11884c0303786da60d5c0b7ff48b1ecc8a685b2
ThiagoKienbaum <thiago.kienbaum@hotmail.com>
2020-01-02 19:29:36 -0300

    first commit

---
+++ aulas/Exercicio01.groovy
@@ -0,0 +1,13 @@
+package aulas
+
+class Exercicio01 {
+	static main (args) {
+		println "Hello World!"
+		
+		String nome = "Thiago"
+		int idade = 29
+		
+		println "O $nome tem $idade anos."
+		
+	}
+}
